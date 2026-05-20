e818d97e2640350391f91d1c0b0525035c19c907
Luciano Miranda <luciano.miranda@viveo.com>
2026-05-19 23:35:06 -0300

    feat: ajustes cors

--- FIAP.Catalog.API/Program.cs
+++ FIAP.Catalog.API/Program.cs
@@ -168,11 +168,11 @@ try
     {
         c.PreSerializeFilters.Add((swagger, httpReq) =>
         {
-            var host   = httpReq.Headers["X-Forwarded-Host"].FirstOrDefault() ?? httpReq.Host.Value;
-            var scheme = httpReq.Headers["X-Forwarded-Proto"].FirstOrDefault() ?? httpReq.Scheme;
+            var gateway = Environment.GetEnvironmentVariable("SWAGGER_GATEWAY_URL")
+                          ?? $"{httpReq.Scheme}://{httpReq.Host}";
             swagger.Servers = new List<OpenApiServer>
             {
-                new() { Url = $"{scheme}://{host}/catalog" }
+                new() { Url = $"{gateway}/catalog" }
             };
         });
     });
